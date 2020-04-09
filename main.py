f5b3ef3a5b65b815aaec88c5f8844f9864f2ace0
Jozef Cibicek <jozef.cibicek@mim.sk>
2020-04-09 19:30:58 +0100

    added main code and utility matrix code

---
+++ main.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+import os
+pd.set_option('display.precision', 10)
+pd.set_option('display.float_format', '{:.2f}'.format)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.width', None)
+warnings.filterwarnings('ignore')
+
+from sklearn.model_selection import train_test_split
+import src.utility_matrix as um
+import src.user_clustering as clt
+import src.svd as sv
+import src.combination as comb
+import src.decision_tree as tree
+
+
+# Load our dataset
+train = pd.read_csv(os.path.join('datasets','1percent.csv'))
+print('number of rows in sample', len(train))
+print(train.head(5))
+
+# Create a dataframe only with columns we need. And sort it by user_id.
+temp = train[['user_id','hotel_cluster','is_booking', 'srch_destination_id']]
+temp.sort_values(by=['user_id'],inplace=True)
+
+# out of 376703 ids, 262231 are unique.
+print('\n Number of unique User IDs:',temp['user_id'].nunique())
+
+# Create Rating column for our dataframe.
+# Where booking == 1, give rating 5 otherwise give rating 1.
+temp['rating'] = np.where((temp['is_booking'] == 1),5,1).astype(float)
+
+
+# Create utility matrix out of temp
+utility_matrix = um.create_utility_matrix(df=temp)
+
+# Then slice it to 1000 rows for further analysis.
+sliced_matrix = utility_matrix[0:1000,:]
+
+#Perform the Cosine Distance Calculation on our sliced matrix.
+normalised = um.get_distance_matrix(sliced_matrix)
+um.plot_hgram(normalised,'sliced_utility_cosine_normalised.png')
+
---
+++ src/utility_matrix.py
@@ -0,0 +1,108 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import spatial
+from sklearn.metrics import pairwise_distances
+import seaborn as sns
+import os
+
+def create_utility_matrix(df):
+    '''
+
+    Generate utility matrix from dataframe;
+    first create blank X users and Y hotel clusters matrix,
+    and fill it with ratings from dataframe df.
+
+    :param df: Dataframe from which you want to create UM.
+    :return: utility matrix
+    '''
+
+    # Create an int Numpy matrix out of 0th, 1th, and 4th columns from dataframe.
+    data_matrix = df.iloc[:, [0, 1, 4]].to_numpy().astype(int)
+
+    # Get the last user id so we know how big our utility matrix will be.
+    last_user_id = int(df.iloc[-1].user_id)  # 1198777
+
+    utility_matrix = np.zeros(shape=(last_user_id + 1, 100))
+    print('our utility matrix has shape', utility_matrix.shape)
+
+    x = [data_matrix[:,0], data_matrix[:,1]]
+    np.add.at(utility_matrix, x, data_matrix[:,2])
+    return utility_matrix
+
+def get_distance_matrix(matrix):
+    '''
+    :param matrix: utility matrix
+    :return: distance matrix with cosine distances
+    '''
+
+    # Calculations done on sliced utility matrix
+
+    # create column vector which has row sums of ratings.
+    row_sums = np.sum(matrix,axis=1)
+
+    #create intermediate column vector with rating sum / N clusters
+    inter_mediate = row_sums / matrix.shape[1]
+
+    # Take away the rating_sum / N clusters column vector
+    # from the sliced matrix
+    matrix = matrix - inter_mediate[:,None]
+
+    # Generate distance matrix using Sklearn library out of
+    # The normalised matrix.
+    dist_out = 1-pairwise_distances(matrix, metric="cosine")
+    return dist_out
+
+def test_cosine():
+    # Calculations done on the array in the excel spreadsheet.
+    test_arr = np.array([[0, 1, 1, 5, 1, 0], [1, 1, 1, 5, 1, 0]])
+    n_clusters = 6
+    print(test_arr)  # Our excel data
+
+    # Column vector with row sums of ratings.
+    row_sums = np.sum(test_arr, axis=1)
+    print(row_sums)
+
+    # Column vector with row sums / N clusters
+    inter_mediate = row_sums / len(test_arr[0])
+    print(inter_mediate)
+
+    # Subtraction
+    # Subtract the column vector with row sums / N clusters from
+    # Our ratings matrix.
+    test_arr = test_arr - inter_mediate[:, None]
+    print(test_arr)
+
+    # Sum of squares
+    ssq = np.sum(test_arr ** 2, axis=1)
+    print(ssq)
+
+    # Squared sum of squares
+    ssq = np.sqrt(ssq)
+    print(ssq)
+
+    # last step. A_k * B_k / squared sum of squares A, ssq B.
+    cd = np.sum(np.prod(test_arr, axis=0)) / np.prod(ssq)
+    print('our cosine similarty is', cd)
+
+    # Test it using Library
+    result = 1 - spatial.distance.cosine(test_arr[0], test_arr[1])
+    assert cd == result
+
+    print('distance matrix')
+    dist_out = 1 - pairwise_distances(test_arr, metric="cosine")
+    print(dist_out)
+
+
+
+def calculate_RAM(dim_x,dim_y):
+    n = np.zeros((dim_x,dim_y))
+    z = (n.size * n.itemsize)
+    print('Matrix takes: ',z/1000000000,'GB RAM')
+
+def plot_hgram(matrix,name):
+    plt.figure(figsize=(20,20))
+    sns.heatmap(matrix)
+    plt.title(name, fontsize=30)
+    plt.savefig(os.path.join('figures',name))
