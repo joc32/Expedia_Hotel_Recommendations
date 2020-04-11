2a27132cb0ea9a92139f5624a26ec66a6dee2ff4
Eleftheria Kouppari <eleftheriakouppari@pc-133-98.customer.ask4.lan>
2020-04-11 17:49:02 +0100

    docstrings

--- src/svd.py
+++ src/svd.py
@@ -3,7 +3,22 @@ import torch
 import numpy as np
 
 def svd(clusters,sliced_matrix):
+    '''
 
+    Performs the Singular Value Decomposition on the utility matrix, to fill in the unknown values.
+    It seeks to find a low-rank matrix X=UV.T, that minimizes the sum-square
+    distance to the fully observed target matrix M.
+    U[N,rank]
+    V[rank,K]
+     where N=the number of user clusters
+           K=the number of hotel clusters
+           rank the number of independent columns in M.
+    It uses Stochastic Gradient Descent to minimize the sum-square distance to M.
+    :param clusters: Is the clusters of users created by the Hierarchical Clustering algorithm.
+    :param sliced_matrix: Is the utility matrix, sliced, due to RAM limitations.
+    :return low- rank matrix X.
+
+    '''
     # We need to compress utility matrix by replacing user_id with user_cluster
     # and remove all rows where user_cluster repeats
     unique_clusters = np.unique(clusters, return_index=True)
@@ -21,40 +36,65 @@ def svd(clusters,sliced_matrix):
     new_clustered=svd(U,V,clustered_matrix)
     return new_clustered
 
+def sgd(M,rank,num_epochs,a,lamda):
 
-# Implements Stochastic Gradient Descent
-# lr: learning rate.
-# lamda: normalisation factor to keep the values from getting to high.
-# num_epochs, the number of iterations the algorithm will perform.
-def sgd(M,rank,num_epochs,lr,lamda):
-  # n: number of user_clusters (112)
-  n=M.shape[0]
-  # k: number of hotel_clusters (100)
-  k=M.shape[1]
-  # Define U and V
-  # U[M,rank] M: number of user clusters
-  # V[rank,K] K: number of hotel clusters.
-  U=torch.rand(n,rank)
-  V=torch.rand(k,rank)
+    '''
 
-  for epoch in range(num_epochs):
-    for r in range(n):
-      for c in range(k):
-        # We are updating U and V for every known value.
-        if M[r][c]>0:
-            e1=M[r][c]-V[c,:].t()@U[r,:]
-            e2=M[r][c]-U[r,:].t()@V[c,:]
-            U[r,:]=U[r,:]+lr*(e1*V[c,:]-lamda*U[r,:])
-            V[c,:]=V[c,:]+lr*(e2*U[r,:]-lamda*V[c,:])
-  return U,V
+     Implements the Strochastic Gradient Descent.
+     Tries to find U and V that minimizes the sum square distance to M.
+     This is done through derivatives of the sum square distance to M,
+     with respect to U and V.
+     U and V are initialised to random values drawn from Gaussian distribution.
+     For the number of epochs:
+        the algorithm iterates over all known values of the M, and calculates the
+        new values of U and V based on the derivatives as shown below:
+        Ui := Ui + α((Mik − VkT Ui)Vk − λUi)
+        Vk := Vk + α((Mik − UiT Vk)Ui − λVk)
 
-# Performs the multiplication of matrices X=U@V.T
+    :param M: utility matrix
+    :param rank: the rank of the utility matrix M.
+    :param num_epochs: number of iterations
+    :param a: the learning rate
+    :param lamda: regularization parameter
+    :return: U,V
+    '''
+    # n: number of user_clusters (112)
+    n=M.shape[0]
+    # k: number of hotel_clusters (100)
+    k=M.shape[1]
+    # Define U and V
+    # U[N,rank] N: number of user clusters
+    # V[rank,K] K: number of hotel clusters
+    U=torch.rand(n,rank)
+    V=torch.rand(k,rank)
+    for epoch in range(num_epochs):
+        for r in range(n):
+            for c in range(k):
+            # We are updating U and V for every known value.
+                if M[r][c]>0:
+                    e1=M[r][c]-V[c,:].t()@U[r,:]
+                    e2=M[r][c]-U[r,:].t()@V[c,:]
+                    U[r,:]=U[r,:]+a*(e1*V[c,:]-lamda*U[r,:])
+                    V[c,:]=V[c,:]+a*(e2*U[r,:]-lamda*V[c,:])
+     return U,V
+
 def construct_svd(U,V,M):
-  n=M.shape[0]
-  k=M.shape[1]
-  new_M = np.zeros([n,k])
-  for r in range(n):
-    for c in range(k):
-      new_M[r][c]=U[r,:]@V[c,:].T
-  return new_M
+    '''
+
+    Calculates matrix X based on:
+        X=U@V.T
+
+    :param U: U
+    :param V: V
+    :param M: utility matrix
+    :return: X
+    '''
+    n=M.shape[0]
+    k=M.shape[1]
+    new_M = np.zeros([n,k])
+    for r in range(n):
+        for c in range(k):
+            # X=U@V.T
+            new_M[r][c]=U[r,:]@V[c,:].T
+    return new_M
 
