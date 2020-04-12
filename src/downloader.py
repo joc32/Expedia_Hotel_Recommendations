a42b614f2993ade530301b8268545de3ddc6d8f4
Jozef Cibicek <jozef.cibicek@mim.sk>
2020-04-12 18:52:30 +0100

    modified downloader py

--- src/downloader.py
+++ src/downloader.py
@@ -1,4 +1,11 @@
 import os
+import zipfile
+
 
 os.system("pip3 install kaggle")
-os.system("kaggle competitions download -c expedia-hotel-recommendations")
+os.system("kaggle competitions download -c expedia-hotel-recommendations -p ../datasets")
+
+with zipfile.ZipFile(os.path.join('..','datasets','expedia-hotel-recommendations.zip'), 'r') as zip_ref:
+    zip_ref.extractall(os.path.join('..','datasets'))
+
+print('\n DATASET READY!!! ')
